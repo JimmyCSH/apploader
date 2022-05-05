e209b102630c87ebaebaa10634b191e2662a57ed
Jimmy <jimmycshaw@hotmail.co.nz>
2022-05-05 23:52:10 +1200

    Program is now able to run files.

--- app.py
+++ app.py
@@ -2,12 +2,32 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 
-from PIL import Image, ImageTk
-
 # Set TKinter Window Properties
 root = tk.Tk()
 root.resizable(width = False, height = False)
 root.title("App Loader")
+
+addedApps = []
+
+def add():
+    # Allow adding of widgets without duplication
+    for widget in appFrame.winfo_children():
+        widget.destroy()
+    
+    # Allow adding .exe files to frame
+    file = filedialog.askopenfilename(initialdir = "/", title = "Select File", 
+        filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    
+    addedApps.append(file)
+
+    # Draw added apps
+    for app in addedApps:
+        label = tk.Label(appFrame, text = app, width = 385)
+        label.pack()
+
+def run():
+    for app in addedApps:
+        os.startfile(app)
 
 # Create Canvas
 screen = tk.Canvas(root, height = 400, width = 700, bg = "#FFB247")
@@ -23,12 +43,12 @@ appFrame.place(relwidth = 0.55, relheight = 0.80, rely = 0.1, relx = 0.4)
 
 # Button for Opening Files
 openFile = tk.Button(logicFrame, text = "Open File", padx = 40, pady = 6, fg = "white", 
-    bg = "#FFB247", borderwidth = 0)
+    bg = "#FFB247", borderwidth = 0, command = add)
 openFile.pack(pady = 30)
 
 # Button for Running Files
 runFile = tk.Button(logicFrame, text = "Run File(s)", padx = 40, pady = 6, fg = "white", 
-    bg = "#FFB247", borderwidth = 0)
+    bg = "#FFB247", borderwidth = 0, command = run)
 runFile.pack()
 
 # Button for Saving Configuration
@@ -40,6 +60,10 @@ saveConfiguration.pack(pady = 30)
 loadConfiguration = tk.Button(logicFrame, text = "Load Configuration", padx = 17, pady = 6, 
     fg = "white", bg = "#FFB247", borderwidth = 0)
 loadConfiguration.pack()
+
+exit = tk.Button(logicFrame, text = "Exit", padx = 20, pady = 20, 
+    fg = "#3D426B", bg = "white", borderwidth = 0)
+exit.pack(pady = 30)
 
 # Run Main Loop
 root.mainloop()
